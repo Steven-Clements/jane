017b1f1679faf7fa0a8dceb3ebf4eace538268c7
Steven <clements.steven07@outlook.com>
2025-12-12 03:19:03 -0700

    fix: type extensions

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,2 +1,2 @@
-export type { default as NormalizationError } from './NormalizationError.js';
-export type { ValidationResult } from './ValidationResult.js';
+export type { default as NormalizationError } from './NormalizationError.ts';
+export type { ValidationResult } from './ValidationResult.ts';
